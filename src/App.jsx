ae407ab894c17728a11c759338d36c0a97668ae9
saraiva142 <saraivajandrade@gmail.com>
2025-05-06 08:48:10 -0300

    Componentizando botão

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,26 @@ import viteLogo from '/vite.svg'
 import { Button } from 'bootstrap'
 import './components/menu'
 import Menu from './components/menu'
+import Card from './components/card'
+import Buttons from './components/buttons/button'
 
 
 function App() {
   const [count, setCount] = useState(0)
+  const handleClick = () => {
+    alert('Botão clicado!');
+  };
 
   return (
     <>
       <Menu />
-      <button className="btn btn-primary">Bootstrap Button</button>
+      
+      <button className="btn btn-primary">Saraiva React</button>
+
+      <Buttons variant = "btn btn-primary" valor="Incluir" />
+      <Buttons variant = "btn btn-danger" valor="Excluir" />
+      <Buttons variant = "btn btn-dark" valor="Clique aqui" acao={handleClick} />
+      
     </>
   )
 }
---
+++ src/components/card.jsx
@@ -0,0 +1,18 @@
+function Card() {
+    return (
+        <>
+            <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                <a href="#" class="card-link">Card link</a>
+                <a href="#" class="card-link">Another link</a>
+            </div>
+            </div>
+        </>
+
+    );
+}
+
+export default Card;
